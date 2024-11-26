580b4f4c189210d5be97d985572aa4d82761383f
ClintonOgembo <clintonogembo70@gmail.com>
2024-11-27 00:19:53 +0300

    first commit

---
+++ variables.py
@@ -0,0 +1,18 @@
+site_name = "Power Learn Project"
+print(site_name)
+
+# Assigning new value to site_name
+site_name = "I love coding 😊"
+print(site_name)
+
+# Assigning multiple values to multiple variables
+a,b,c = 4, 8, "Hello world"
+print(a) # prints 4
+print(b) # prints 8
+print(c) # prints Hello World
+
+# python is case sensitive
+num = 88
+Num = 510
+print(num) #88
+print(Num) #510
