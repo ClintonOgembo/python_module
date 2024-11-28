cf502bed8db158d1cf1726387889ee74903953b8
ClintonOgembo <clintonogembo70@gmail.com>
2024-11-28 22:50:08 +0300

    arithmetics done

---
+++ arithmeticOperators.py
@@ -0,0 +1,20 @@
+# subtraction
+sub = 10 - 5
+print(sub)
+
+# addition
+add = 5 + 2 
+print(add)
+
+# exponentiation(power)
+pwr = 4 ** 2
+print(pwr)
+
+# float division
+div = 4//2 
+print(div)
+
+# floor division 
+fdiv = 10 // 3
+print(fdiv)
+
