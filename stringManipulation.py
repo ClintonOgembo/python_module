4fc82b1766ca758e32a93b9c810ee6ab5d752789
ClintonOgembo <clintonogembo70@gmail.com>
2024-12-01 12:17:02 +0300

    str manipulation not yet done

---
+++ stringManipulation.py
@@ -0,0 +1,30 @@
+# a string is a sequence of characters.
+string1 = "Python"
+ 
+# Access string characters 
+# 1. indexing
+print(string1[0]) # prints "p"
+
+# 2. Negative indexing
+print(string1[-2]) # Prints 'o'
+
+# slicing
+print(string1[0:2]) #prints 'py'
+
+# strings are immutable
+# but we can assign a new value to the same variable
+text = 'Hello'
+print(text)
+
+text = 'Hello World!'
+print(text)
+
+# create multiline String
+# we triple """ or '''
+message = '''
+my name is clinton
+i like football and 
+am a software engineer 
+'''
+print(message)
+
