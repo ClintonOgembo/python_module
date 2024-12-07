3ad50b68e7223042f37b3fbcafca8de023ff24d9
ClintonOgembo <clintonogembo70@gmail.com>
2024-12-07 22:53:35 +0300

    not easy

--- controlFlowStructures.py
+++ controlFlowStructures.py
@@ -1,5 +1,12 @@
 # Example of a basic if statement
-temperature = 30
+temperature = 8
+
 
 if temperature > 25:
     print("It's a hot day!")
+elif temperature > 15:
+    print("It's a warm day!")
+elif temperature > 9:
+    print("It's a cool day!")
+else:
+    print("It's a cold day!")
