fd5055fad030710d2c7c455f4bd61c17c8d71c04
ClintonOgembo <clintonogembo70@gmail.com>
2024-12-16 22:38:50 +0300

    ice breaker

--- dictionaries.py
+++ dictionaries.py
@@ -1,39 +1,53 @@
-# Python dictionary is an ordered collection of items. It stores elements in key/value pairs
-capitalCity = {"Kenya" : "Nairobi", "Uganda" : "Kampala", "Tanzania" : "Dar el Salam", "Ethiopia" : "Adis Ababa"}
-print(capitalCity)
+# A dictionary is an ordered collection of items stored as key/value pairs.
+# Below is a dictionary of some countries and their capital cities.
+capitalCity = {
+    "Kenya": "Nairobi", 
+    "Uganda": "Kampala", 
+    "Tanzania": "Dar es Salaam", 
+    "Ethiopia": "Addis Ababa"
+}
+print("Initial Dictionary of Capital Cities:", capitalCity)
 
-# we can also have keys of diff data type and values of diff data type
-numbers = {1:"One", 2:"Two", 3:"Three", 4:"Four"}
-print(numbers)
+# Dictionaries can have keys and values of different data types.
+# Example: A dictionary mapping integers to their word representation.
+numbers = {
+    1: "One", 
+    2: "Two", 
+    3: "Three", 
+    4: "Four"
+}
+print("Number Dictionary:", numbers)
 
-# Adding elements to a dictionary
+# Adding a new key-value pair to the dictionary
 capitalCity["Japan"] = "Tokyo"
-print("Updated Dictionary: ",capitalCity)
+print("Updated Dictionary of Capital Cities:", capitalCity)
 
-# change values of a dict
-del numbers[1] # deleting
-print("Update numbers: ", numbers)
-numbers[2] = "two" # changing "Two" with "two"
-print(numbers)
+# Modifying or deleting items in a dictionary
+del numbers[1]  # Deletes the key-value pair with key 1
+print("Numbers Dictionary after Deletion:", numbers)
 
-# accessing elements from a Dict
-# we use the keys to access the corresponding values
-print(capitalCity["Kenya"]) # prints Nairobi
-print(capitalCity["Uganda"]) # prints Kampala
+numbers[2] = "two"  # Modifies the value of key 2
+print("Numbers Dictionary after Modification:", numbers)
 
-# we can also delete a whole dictionary
+# Accessing values in a dictionary using their keys
+# Example: Accessing capital cities
+print("Capital of Kenya:", capitalCity["Kenya"])
+print("Capital of Uganda:", capitalCity["Uganda"])
+
+# Deleting the entire dictionary (commented out to avoid errors)
 # del numbers
-# print(numbers)
+# print(numbers)  # This will raise an error if 'numbers' is deleted
 
-# length of a dict
+# Finding the number of items in a dictionary
 dict_length = len(capitalCity)
-print(dict_length)
+print("Number of Items in Capital Cities Dictionary:", dict_length)
 
-# dictionary membership test
-# we use the keyword "in" and we only test the keys not the values
-print("Ethiopia" in capitalCity) # returns true
-print("Somalia" in capitalCity) # returns false, since the key Somalia does not exist in the dict
+# Checking if a key exists in the dictionary (membership test)
+# Note: The "in" keyword tests for keys only, not values.
+print("Is 'Ethiopia' a key in the dictionary?", "Ethiopia" in capitalCity)  # True
+print("Is 'Somalia' a key in the dictionary?", "Somalia" in capitalCity)   # False
 
-# iterating through each key using a loop
-for r in capitalCity:
-    print('key', r)
+# Iterating through keys in the dictionary using a loop
+print("Keys in Capital Cities Dictionary:")
+for key in capitalCity:
+    print("Key:", key)
