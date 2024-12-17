5df272b32cdc526cfa3abe675ce24591f4e42ae6
ClintonOgembo <clintonogembo70@gmail.com>
2024-12-17 22:52:55 +0300

    ice breaker

--- fileHandling_&_ExceptionHandling.py
+++ fileHandling_&_ExceptionHandling.py
@@ -1,1 +1,16 @@
 # file handling
+try:
+    with open("nonexistent.txt", "r") as file:
+        data = file.read()
+except FileNotFoundError:
+    print("File not found. Please check the filename.")
+
+
+
+try:
+    file = open("sample.txt", "r")
+    data = file.read()
+except FileNotFoundError:
+    print("File not found.")
+finally:
+    file.close()
